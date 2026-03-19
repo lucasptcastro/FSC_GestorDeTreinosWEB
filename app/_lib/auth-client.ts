ab42baf8896f7d0e7ff47591392652ef72f7ec5b
lucasptcastro <lucasptcastro@gmail.com>
2026-03-18 22:02:50 -0300

    feat: add sign in with better auth

--- app/_lib/auth-client.ts
+++ app/_lib/auth-client.ts
@@ -1,9 +1,5 @@
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
-
-authClient.signIn.social({
-  provider: "google",
 });
