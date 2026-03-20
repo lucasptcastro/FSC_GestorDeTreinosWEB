12078120c7ad2eb4930add7d54741ce90012e871
lucasptcastro <lucasptcastro@gmail.com>
2026-03-20 15:00:08 -0300

    feat: add chatbot widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Anton, Geist, Geist_Mono, Inter_Tight } from "next/font/google";
 import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Chat } from "@/app/_components/chat";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +41,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${interTight.variable} ${anton.variable} antialiased`}
       >
-        {children}
+        <NuqsAdapter>
+          {children}
+          <Suspense>
+            <Chat />
+          </Suspense>
+        </NuqsAdapter>
       </body>
     </html>
   );
